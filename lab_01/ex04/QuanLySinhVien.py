dfb38e3172c7e7ed29cce5779ceafa84fcfdb81f
DangThanhHoa <dthanhhoa2506@gmail.com>
2026-05-14 10:51:04 +0700

    move ex03 into lab_01

---
+++ lab_01/ex04/QuanLySinhVien.py
@@ -0,0 +1,43 @@
+from SinhVien import SinhVien
+
+class QuanLySinhVien:
+    listSinhVien = []
+
+    def generateID(self):
+        maxId = 1
+        if (self.soLuongSinhVien() > 0):
+            maxId = self.listSinhVien[0]._id
+            for sv in self.listSinhVien:
+                if (maxId < sv._id):
+                    maxId = sv._id
+            maxId = maxId + 1
+        return maxId
+
+    def soLuongSinhVien(self):
+        return self.listSinhVien.__len__()
+
+    def nhapSinhVien(self):
+        svId = self.generateID()
+        name = input("Nhap ten sinh vien: ")
+        sex = input("Nhap gioi tinh sinh vien: ")
+        major = input("Nhap chuyen nganh cua sinh vien: ")
+        diemTB = float(input("Nhap diem cua sinh vien: "))
+        sv = SinhVien(svId, name, sex, major, diemTB)
+        self.xepLoaiHocLuc(sv)
+        self.listSinhVien.append(sv)
+
+    def updateSinhVien(self, ID):
+        sv: SinhVien = self.findByID(ID)
+        if (sv != None):
+            name = input("Nhap ten sinh vien: ")
+            sex = input("Nhap gioi tinh sinh vien: ")
+            major = int(input("Nhap chuyen nganh cua sinh vien: "))
+            diemTB = float(input("Nhap diem cua sinh vien: "))
+
+            sv._name = name
+            sv._sex = sex
+            sv._major = major
+            sv._diemTB = diemTB
+
+            self.xepLoaiHocLuc(sv)
+            
